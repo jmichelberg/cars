e65c0d324d7d1b3f2c30a7f3d7891c6cb691a71f
Josh Michelberg <michelbe@chapman.edu>
2018-11-28 11:36:22 -0800

    lists cars

---
+++ cars.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# cars.sh
+# Josh Michelberg
+
+while true
+do
+	echo -e "1. Add a car to the list."
+	echo -e "2. List all cars."
+	echo -e "3. Quit the program."
+	echo -e "Please enter a number."
+	read command
+	case "$command" in
+		"1") echo "Add a car to the list."
+			echo "What year was the car made?"
+			read year
+			echo "What is the make of the car?"
+			read make
+			echo "What is the model of the car?"
+			read model
+			echo $year:$make:$model >> My_old_cars
+;;
+		"2") echo "List all cars."
+			sort My_old_cars
+;;
+		"3") break
+;;
+		*) echo "Invalid input. Enter a number 1-3."
+esac
+done 
